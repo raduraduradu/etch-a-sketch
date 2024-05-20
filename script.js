48dab66dedd2297b410bcddc59bb117c0ec0a568
Radu <radu.stanciu2016@gmail.com>
2024-05-20 19:12:09 +0300

    made working resize grid button

--- script.js
+++ script.js
@@ -9,12 +9,24 @@ function randomInt(min, max) {
   return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
 }
 
+function getNewSize() {
+  let size = Number(prompt("Enter a number (under 100)"));
+  while(isNaN(size) || size > 100)
+  {
+    alert("Invalid input");
+    size = Number(prompt("Enter a number (under 100)"));
+  }
+  return Math.round(size);
+}
+
 function createGrid(gridSize) {
+  pixels = [];
+  pxHeight = container.clientHeight / gridSize;
+  pxWidth = container.clientWidth / gridSize;
   for(let i = 0; i < gridSize**2; i++) {
     pixels[i] = document.createElement("div");
     pixels[i].classList.add("pixel");
-    pixels[i].style.height = `${container.clientHeight / gridSize}px`;
-    pixels[i].style.width = `${container.clientWidth / gridSize}px`;
+    pixels[i].setAttribute("style", `height: ${pxHeight}px; width: ${pxWidth}px;`)
     pixels[i].addEventListener("mouseover", () => {
       pixels[i].style.backgroundColor = `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
     })
@@ -24,3 +36,12 @@ function createGrid(gridSize) {
 }
 
 createGrid(DEFAULT_GRID_SIZE);
+
+const btnResize = document.querySelector("button#btn-resize");
+btnResize.addEventListener("click", () => {
+  let newSize = getNewSize();
+  for(let i = 0; i < pixels.length; i++) {
+    pixels[i].remove();
+  }
+  createGrid(newSize);
+})
